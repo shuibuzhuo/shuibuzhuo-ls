074c823122c24c009834e1572caa6bba202de699
shuibuzhuo <2301140231@qq.com>
2023-12-02 18:30:34 +0800

    feat: 遍历文件

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
+const fs = require("fs");
 const parse = require("./parseArgs");
 const { args, isAll, isList } = parse();
 
-console.log(args, isAll, isList);
+const dir = process.cwd();
+
+if (!isAll && !isList) {
+  let files = fs.readdirSync(dir);
+  files = files.filter((file) => file.indexOf(".") !== 0);
+  let output = "";
+  files.forEach((file) => (output += file + "      "));
+  console.log(output);
+}
